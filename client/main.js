d3e435e5762cd82a021f968de88994abb43c563a
zyadomer999 <zyadomer999@gmail.com>
2020-09-07 23:43:51 +0300

    adsafas

--- client/main.js
+++ client/main.js
@@ -141,3 +141,19 @@
 // a.href = url;
 // a.download = "fileName.txt";
 // a.click();
+
+var video = document.getElementById("video");
+var videoSrc = "./server";
+if (Hls.isSupported()) {
+  var hls = new Hls();
+  hls.loadSource(videoSrc);
+  hls.attachMedia(video);
+  hls.on(Hls.Events.MANIFEST_PARSED, function () {
+    video.play();
+  });
+} else if (video.canPlayType("application/vnd.apple.mpegurl")) {
+  video.src = videoSrc;
+  video.addEventListener("loadedmetadata", function () {
+    video.play();
+  });
+}
