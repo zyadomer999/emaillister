add88bddfff34beb2fce0bdc329f187ea3d690fe
zyadomer999 <zyadomer999@gmail.com>
2020-09-24 10:07:19 +0300

    sdfsd

--- client/main.js
+++ client/main.js
@@ -1,25 +1,60 @@
-const video = document.querySelector("video");
-function server(date, n) {
-  let time = `${date}`
-    .split("2020 ")[1]
-    .split(" ")[0]
-    .split(":")
-    .join("");
-  time = parseInt(time) + n;
-  return `${time * time * time - time * time}`;
+const allEmails = document.querySelector("#allEmails");
+
+const emailContainer = document.createElement("div");
+emailContainer.setAttribute("class", "emailContainer");
+
+fetch("/server").then((response) => {
+  response.text().then((e) => {
+    data = JSON.parse(e);
+    for (let a = 0; a < data.length; ++a) {
+      if (data[a].created == "no") {
+        emailContainer.innerHTML = `<div><h1>${data[a].email}</h1><h3 onclick='copy(this)'>Copy</h3></div><div><h1>${data[a].password}</h1><h3 onclick='copy(this)'>Copy</h3></div><div><h3 onclick=finish(this)>Finish</h3></div>`;
+        let template = emailContainer.cloneNode(true);
+        allEmails.insertAdjacentElement("beforeend", template);
+      }
+    }
+  });
+});
+
+function copy(copyBtn) {
+  copyBtn.style.backgroundColor = "rgb(233,47,47)";
+  let email = copyBtn.previousElementSibling.textContent;
+  let input = document.createElement("input");
+  input.setAttribute("value", email);
+  document.body.appendChild(input);
+  input.select();
+  document.execCommand("copy");
+  document.body.removeChild(input);
 }
 
-video.addEventListener("canplay", function () {
-  video.play();
-});
-if (Hls.isSupported()) {
-  var hls = new Hls();
-  hls.loadSource(`/server${server(new Date().toUTCString(), 0)}`);
-  hls.attachMedia(video);
-  hls.on(Hls.Events.MANIFEST_PARSED, function () {
-    video.play();
-  });
-} else if (video.canPlayType("application/vnd.apple.mpegurl")) {
-  video.src = `/server${server(new Date().toUTCString(), 0)}`;
-  video.play();
+function finish(finishBtn) {
+  copyPasswordBtn =
+    finishBtn.parentElement.previousElementSibling.children[1].style
+      .backgroundColor;
+  copyEmailBtn =
+    finishBtn.parentElement.previousElementSibling
+      .previousElementSibling.children[1].style.backgroundColor;
+  if (
+    copyPasswordBtn == "rgb(233, 47, 47)" &&
+    copyEmailBtn == "rgb(233, 47, 47)"
+  ) {
+    finishBtn.textContent = "Created";
+    finishBtn.style.backgroundColor = "#9019D1";
+    fetch("/server", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email:
+          finishBtn.parentElement.previousElementSibling
+            .previousElementSibling.children[0].textContent,
+        password:
+          finishBtn.parentElement.previousElementSibling.children[0]
+            .textContent,
+        created: "yes",
+      }),
+    });
+  }
 }
--- index.js
+++ index.js
@@ -1,136 +1,48 @@
 const port = process.env.PORT || 5000;
 const express = require("express");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const fetch = require("node-fetch");
 const app = express();
 
-async function getFreshToken() {
-  let token;
-  let accessToken = {
-    client_id:
-      "645222427076-356d5p7n94bt0pgnvfesmd9j6dqu6ld6.apps.googleusercontent.com",
-    client_secret: "jrAeGeRDcG9UJsptLbMaVEIL",
-    refresh_token:
-      "1//03Rt8s0JRhs-SCgYIARAAGAMSNwF-L9IrGzOXvuNlYNJUeuGAasQYfrfVhXoI3KNWfHq06w2wzKt0fPGy3aK5vWajz3QyMXNrr8A",
-    grant_type: "refresh_token",
-  };
-  let formBody = [];
-  for (let property in accessToken) {
-    let encodedKey = encodeURIComponent(property);
-    let encodedValue = encodeURIComponent(accessToken[property]);
-    formBody.push(encodedKey + "=" + encodedValue);
+app.use(express.json());
+app.use(express.urlencoded());
+
+let data = [
+  {
+    email: "mike456ponder@outlook.com",
+    password: "saman1998",
+    created: "no",
+  },
+  {
+    email: "cdfzcp4knkd1s32@outlook.com",
+    password: "ek~PWv>jRgf1{D7O}[|{R6",
+    created: "no",
+  },
+  {
+    email: "x48tv4one104lex-l9zuvz@outlook.com",
+    password: "maV5exe!SgYkCO@xj9{k",
+    created: "no",
+  },
+  {
+    email: "Krish3366Saunders8873@outlook.com",
+    password: "Manson8238Roray",
+    created: "no",
+  },
+];
+app.get("/server", function (req, res) {
+  res.send(JSON.stringify(data));
+});
+app.post("/server", function (req, res) {
+  toBeCreated = "";
+  number = 0;
+  for (let a = 0; a < data.length; ++a) {
+    if (data[a].created == "no") {
+      toBeCreated = data[a].email;
+      number = a;
+      break;
+    }
   }
-  formBody = formBody.join("&");
-  let response;
-  await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: formBody,
-  }).then((e) => {
-    response = e;
-  });
-  await response.json().then((e) => {
-    token = e.access_token;
-  });
-  return token;
-}
-
-function server(date, n) {
-  let time = `${date}`
-    .split("2020 ")[1]
-    .split(" ")[0]
-    .split(":")
-    .join("");
-  time = parseInt(time) + n;
-  return `${time * time * time - time * time}`;
-}
-
-let authToken;
-async function start() {
-  await getFreshToken().then((access_token) => {
-    authToken = access_token;
-  });
-  app.use(
-    `/server${server(new Date().toUTCString(), -2)}`,
-    initiate(
-      "https://www.googleapis.com/drive/v3/files/1-uqjC3xUjPdI_empNIKF2mzplRNdViyP?alt=media&key=AIzaSyBPR7Y-KhcVd0mJYOXMrB6bOo6q_0mdNE0"
-    )
-  );
-  app.use(
-    `/server${server(new Date().toUTCString(), -1)}`,
-    initiate(
-      "https://www.googleapis.com/drive/v3/files/1-uqjC3xUjPdI_empNIKF2mzplRNdViyP?alt=media&key=AIzaSyBPR7Y-KhcVd0mJYOXMrB6bOo6q_0mdNE0"
-    )
-  );
-  app.use(
-    `/server${server(new Date().toUTCString(), 0)}`,
-    initiate(
-      "https://www.googleapis.com/drive/v3/files/1-uqjC3xUjPdI_empNIKF2mzplRNdViyP?alt=media&key=AIzaSyBPR7Y-KhcVd0mJYOXMrB6bOo6q_0mdNE0"
-    )
-  );
-  app.use(
-    `/server${server(new Date().toUTCString(), +1)}`,
-    initiate(
-      "https://www.googleapis.com/drive/v3/files/1-uqjC3xUjPdI_empNIKF2mzplRNdViyP?alt=media&key=AIzaSyBPR7Y-KhcVd0mJYOXMrB6bOo6q_0mdNE0"
-    )
-  );
-  app.use(
-    `/server${server(new Date().toUTCString(), +2)}`,
-    initiate(
-      "https://www.googleapis.com/drive/v3/files/1-uqjC3xUjPdI_empNIKF2mzplRNdViyP?alt=media&key=AIzaSyBPR7Y-KhcVd0mJYOXMrB6bOo6q_0mdNE0"
-    )
-  );
-  setInterval(function () {
-    app._router.stack[3].regexp = new RegExp(
-      `^\/server${server(new Date().toUTCString(), -2)}\/?(?=\/|$)`,
-      "i"
-    );
-    app._router.stack[4].regexp = new RegExp(
-      `^\/server${server(new Date().toUTCString(), -1)}\/?(?=\/|$)`,
-      "i"
-    );
-    app._router.stack[5].regexp = new RegExp(
-      `^\/server${server(new Date().toUTCString(), 0)}\/?(?=\/|$)`,
-      "i"
-    );
-    app._router.stack[6].regexp = new RegExp(
-      `^\/server${server(new Date().toUTCString(), 1)}\/?(?=\/|$)`,
-      "i"
-    );
-    app._router.stack[7].regexp = new RegExp(
-      `^\/server${server(new Date().toUTCString(), 2)}\/?(?=\/|$)`,
-      "i"
-    );
-  }, 1000);
-}
-
-function initiate(link) {
-  const options = {
-    target: link,
-    changeOrigin: true,
-    ws: true,
-    pathRewrite: {
-      "^/api/old-path": "/api/new-path",
-      "^/api/remove/path": "/path",
-    },
-    router: {
-      "dev.localhost:5501": "http://localhost:5501",
-    },
-    headers: {
-      origin: "noWay",
-    },
-    onProxyReq(proxyReq, req, res) {
-      proxyReq.setHeader("Authorization", "Bearer " + authToken);
-    },
-  };
-  return createProxyMiddleware(options);
-}
-
-start();
-setInterval(start, 3300000);
-app.setMaxListeners(1000);
+  if (req.body.email == toBeCreated) {
+    data[number].created = "yes";
+  }
+});
 app.use(express.static("client"));
 app.listen(port);
